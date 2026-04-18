0ce31da7274912f7b453faeaa3eed33ae4abed36
nidoham28 <nidohamondol@gmail.com>
2026-04-18 21:00:42 +0600

    Improved it

--- composeApp/src/commonMain/kotlin/com/nidoham/bondhu/App.kt
+++ composeApp/src/commonMain/kotlin/com/nidoham/bondhu/App.kt
@@ -1,6 +1,7 @@
 package com.nidoham.bondhu
 
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.tooling.preview.Preview
 
@@ -8,5 +9,8 @@ import androidx.compose.ui.tooling.preview.Preview
 @Preview
 fun App() {
     MaterialTheme {
+        Text(
+            "OK"
+        )
     }
 }
