de468a50dd9b4a5dbe0e0ceaf4576f78357d3f19
nidoham28 <nidohamondol@gmail.com>
2026-04-18 21:05:51 +0600

    Improved it

--- composeApp/src/commonMain/kotlin/com/nidoham/bondhu/Main.kt
+++ composeApp/src/commonMain/kotlin/com/nidoham/bondhu/Main.kt
@@ -1,16 +1,12 @@
 package com.nidoham.bondhu
 
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
-fun App() {
+fun Main() {
     MaterialTheme {
-        Text(
-            "OK"
-        )
     }
 }
--- composeApp/src/iosMain/kotlin/com/nidoham/bondhu/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/nidoham/bondhu/MainViewController.kt
@@ -2,4 +2,6 @@ package com.nidoham.bondhu
 
 import androidx.compose.ui.window.ComposeUIViewController
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    Main()
+}
