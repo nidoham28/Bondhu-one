c3305d1f78becb6dc2acc11aba8c9adcabfa6fdd
nidoham28 <nidohamondol@gmail.com>
2026-04-21 07:51:32 +0600

    Improved it

--- composeApp/src/iosMain/kotlin/com/nidoham/bondhu/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/nidoham/bondhu/MainViewController.kt
@@ -1,6 +1,7 @@
 package com.nidoham.bondhu
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.nidoham.bondhu.theme.BondhuTheme
 
 fun MainViewController() = ComposeUIViewController {
     BondhuTheme {
